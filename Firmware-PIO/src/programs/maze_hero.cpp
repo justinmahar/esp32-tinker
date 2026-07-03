151a20fbbb6f8f4b268a87a5fa2565f347f047b3
Justin Mahar <contact@justinmahar.com>
2026-07-02 22:45:44 -0500

    Comment

--- Firmware-PIO/src/programs/maze_hero.cpp
+++ Firmware-PIO/src/programs/maze_hero.cpp
@@ -1,11 +1,7 @@
 #include "maze_hero.h"
 
 #include "maze_hero/maze_hero_game.h"
+// Start the Maze Hero game
+void mazeHeroStart(const ProgramConfig &cfg) { MazeHero::start(cfg); }
 
-void mazeHeroStart(const ProgramConfig &cfg) {
-  MazeHero::start(cfg);
-}
-
-void mazeHeroTick(const ProgramConfig &cfg) {
-  MazeHero::tick(cfg);
-}
+void mazeHeroTick(const ProgramConfig &cfg) { MazeHero::tick(cfg); }
